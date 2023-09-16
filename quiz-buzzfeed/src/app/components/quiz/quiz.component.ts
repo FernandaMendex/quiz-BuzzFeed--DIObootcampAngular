fd434279e39b48b45e9547dfb1aad37713030870
FernandaMendex <fernandamendesq@hotmail.com>
2023-09-16 18:09:24 -0300

    refactor: criação da lógica das perguntas

--- quiz-buzzfeed/src/app/components/quiz/quiz.component.ts
+++ quiz-buzzfeed/src/app/components/quiz/quiz.component.ts
@@ -1,5 +1,6 @@
 import { NgIf } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quiz',
@@ -24,7 +25,29 @@ export class QuizComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
 
+      this.questions = quizz_questions.questions
+      this.question = this.questions[this.questionIndex]
+
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+  }
+
+  async next(){
+    this.questionIndex+=1
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.question = this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+    }
   }
 
 }
